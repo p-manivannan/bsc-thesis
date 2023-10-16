d8c24d7fd568716bfd3547e65ae5dea5db1b272e
Prithviraj Manivannan <p.manivannan@student.rug.nl>
2023-10-16 17:13:20 +0200

    bruh

--- hyperparam_tuning.py
+++ hyperparam_tuning.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import train_test_split
 from keras.callbacks import EarlyStopping
-from models import *
+from models_bachelors import *
 from file_functions import *
 
 '''
