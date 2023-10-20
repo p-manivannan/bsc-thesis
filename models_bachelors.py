8bdde7f6ddacc8bdc5c05d39ad856159649a6df3
Prithviraj Manivannan <p.manivannan@student.rug.nl>
2023-10-20 12:30:10 +0200

    k

--- models_bachelors.py
+++ models_bachelors.py
@@ -108,6 +108,45 @@ def tune_model(x_train, x_val, y_train, y_val, method, callbacks):
     return tuner
 
     
-    
-print('bogos binted')
-    
+def create_model(drop_rates, method):
+    C = 22          # Number of electrodes
+    T = 1125        # Time samples of network input
+
+    k_1 = 40        # K is number of convolutional kernels. SUBJECT TO HYPERPARAM TUNING
+    f_1 = 25        # F is kernel size SUBJECT TO HYPERPARAM TUNING
+
+    k_2 = 40
+    f_2 = C
+
+    f_p = (1, 75)   # Fp is pooling size
+    s_p = (1, 15)   # Sp is pool stride
+
+    Nc = 4          # Number of classes
+
+    model = keras.models.Sequential()
+    model.add(keras.layers.Conv2D(f_1,  k_1, padding = 'SAME',
+                                activation="linear",
+                                input_shape = (C, T, 1),
+                                kernel_constraint = max_norm(2)))
+    model.add(keras.layers.Conv2D(f_2,  k_2, padding = 'SAME',
+                                input_shape = (1, C, T),
+                                activation="linear",
+                                kernel_constraint = max_norm(2)))
+    model.add(keras.layers.BatchNormalization(momentum=0.9, epsilon=0.00001))
+    model.add(keras.layers.Activation(square))
+    model.add(keras.layers.AveragePooling2D(pool_size= f_p, strides= s_p))
+    model.add(keras.layers.Activation(log))
+    if method == 'mcdropconnect':
+        model.add(DropConnectDense(22, drop_rates))
+    elif method == 'mcdropout':
+        model.add(StochasticDropout(drop_rates))
+    else:
+        model.add(keras.layers.Dropout(drop_rates))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(Nc, activation='softmax', kernel_constraint = max_norm(0.5)))
+
+    optimizer = keras.optimizers.Adam(learning_rate=1e-4)
+    model.compile(loss="categorical_crossentropy",
+                  optimizer=optimizer, metrics=["accuracy"])
+    return model
+
